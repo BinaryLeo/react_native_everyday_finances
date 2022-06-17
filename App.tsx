68dfe68e302a62f97cd65a906011f9a76bd0079c
binaryleo <leomouracss@fastmail.com>
2022-06-16 23:26:07 -0300

    chore: temporary style

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
   return (
-  
-      <Dashboard/>
+    <View style={styles.container}>
+      <Dashboard />
+    </View>
 
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
