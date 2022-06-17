90a2bcf0604285f9df0b6fdbc5f599e3714e3093
binaryleo <leomouracss@fastmail.com>
2022-06-17 18:32:35 -0300

    chore: add dashboard elements

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Text } from "react-native";
-import { Container, Header } from "./styles";
+import { 
+  Container,
+   Header,
+   UserInfo,
+   Photo,
+   User,
+   UserGreeting,
+   UserName,
+   UserWrapper,
+
+   } from "./styles";
 
 interface Props {
   onLayout: () => void;
@@ -8,7 +18,18 @@ interface Props {
 export function Dashboard({ onLayout }: Props) {
   return (
     <Container onLayout={onLayout}>
-      <Header></Header>
+      <Header>
+        <UserWrapper>
+        <UserInfo>
+          <Photo 
+          source={{uri:'https://github.com/binaryleo.png'}} />
+          <User>
+            <UserGreeting>Olá,</UserGreeting>
+            <UserName>João</UserName>
+          </User>
+        </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,13 +1,41 @@
 import styled from "styled-components/native";
-import {RFPercentage} from 'react-native-responsive-fontsize';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 export const Container = styled.View`
   flex: 1;
   background-color: #fff;
   background-color: ${({ theme }) => theme.colors.background};
 `;
- export const Header = styled.View`
- width: 100%;
- height: ${RFPercentage(42)}px;
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const UserWrapper = styled.View`
 
- background-color: ${({ theme }) => theme.colors.primary};
-`
+`;
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: ${RFValue(7.5)}px;
+  margin-right: ${RFPercentage(2)}px;
+`;
+export const User = styled.View`
+  margin-left: ${RFPercentage(2)}px;
+`;
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
