c0dab6fafcf3ad7a244d769dfbe0c7acd841a3d7
binaryleo <leomouracss@fastmail.com>
2022-06-18 22:31:17 -0300

    chore: Highlight cards styled components

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,21 +1,37 @@
-import styled from 'styled-components/native';
-import {Feather} from '@expo/vector-icons';
+import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 export const Container = styled.View`
-background-color: ${({ theme }) => theme.colors.shape};
-width: ${RFValue(300)}px;
-padding:19px 23px;
-padding-bottom: ${RFValue(42)}px;
-border-top-left-radius: 5px;
-border-top-right-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.shape};
+  width: ${RFValue(300)}px;
+  padding: 19px 23px;
+  padding-bottom: ${RFValue(42)}px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
 `;
 
-export const Header = styled.View``
-export const Title = styled.Text``
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(20)}px;
+`;
 export const Icon = styled(Feather)`
- color: ${({ theme }) => theme.colors.title};
-  font-size: ${RFValue(24)}px;
-`
-export const Footer = styled.View``
-export const Amount = styled.Text``
-export const LastTransaction = styled.Text``
+  color: ${({ theme }) => theme.colors.success};
+  font-size: ${RFValue(40)}px; 
+`;
+export const Footer = styled.View``;
+export const Amount = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(32)}px;
+  color: ${({ theme }) => theme.colors.title};
+  margin-top: 38px;
+`;
+export const LastTransaction = styled.Text` 
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(12)}px;
+  color: ${({ theme }) => theme.colors.secondary};
+`;
